bef1e87dbac76b0fd32b9b8f658724ac510ea24d
bizmedia1 <victorisrael539@gmail.com>
2026-06-23 15:15:19 +0100

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,11 +1,75 @@
-export default async function handler(req,res){
+import axios from "axios";
 
-res.status(200).json({
+export default async function handler(req, res) {
 
-success:true,
+  try {
 
-message:"Nextel API Working"
+    const reference =
+      "NEXTEL-" + Date.now();
 
-});
+    const customerId =
+      Date.now().toString();
+
+    const response =
+      await axios.post(
+
+        "https://api-v1.aspfiy.com/reserve-palmpay/",
+
+        {
+          email:
+            `${customerId}@nextel.app`,
+
+          reference,
+
+          firstName:
+            "Registrant",
+
+          lastName:
+            customerId.slice(-6),
+
+          webhookUrl:
+            "https://YOUR-VERCEL-DOMAIN.vercel.app/api/webhook",
+
+          phone:
+            "09000000000"
+        },
+
+        {
+          headers: {
+            Authorization:
+              `Bearer ${process.env.ASPFIY_SECRET_KEY}`,
+
+            "Content-Type":
+              "application/json"
+          }
+        }
+      );
+
+    return res.status(200).json({
+
+      success: true,
+
+      reference,
+
+      data: response.data
+
+    });
+
+  } catch (error) {
+
+    console.log(
+      error.response?.data || error.message
+    );
+
+    return res.status(500).json({
+
+      success: false,
+
+      error:
+        error.response?.data || error.message
+
+    });
+
+  }
 
 }
