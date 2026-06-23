d5125a14b23d325b0b31ac32891603c7021e5870
bizmedia1 <victorisrael539@gmail.com>
2026-06-23 19:28:33 +0100

    Update payment-status.js

--- api/payment-status.js
+++ api/payment-status.js
@@ -4,21 +4,33 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
 
-  const { accountNumber } = req.query;
+  const accountNumber =
+    req.query.accountNumber;
 
   if (!accountNumber) {
+
     return res.status(400).json({
       paid: false
     });
+
   }
 
-  const payment = await redis.get(
-    `payment:${accountNumber}`
-  );
+  const payment =
+    await redis.get(
+      `payment:${accountNumber}`
+    );
+
+  if (payment) {
+
+    return res.status(200).json({
+      paid: true,
+      payment
+    });
+
+  }
 
   return res.status(200).json({
-    paid: !!payment,
-    payment
+    paid: false
   });
 
 }
