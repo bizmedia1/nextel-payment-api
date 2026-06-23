5918b56ca906e680eba0a6356e0741e58041ff2f
bizmedia1 <victorisrael539@gmail.com>
2026-06-23 19:10:51 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -4,8 +4,16 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
 
+  if (req.method === "GET") {
+    return res.status(200).json({
+      message: "Webhook live"
+    });
+  }
+
   if (req.method !== "POST") {
-    return res.status(405).end();
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
   }
 
   try {
