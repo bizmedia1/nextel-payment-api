4ea395e984159fa9b0026be84be34dd083b61642
bizmedia1 <victorisrael539@gmail.com>
2026-06-23 20:50:53 +0100

    Update payment-status.js

--- api/payment-status.js
+++ api/payment-status.js
@@ -4,15 +4,32 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
 
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const accountNumber =
     req.query.accountNumber;
 
   if (!accountNumber) {
-
     return res.status(400).json({
-      paid: false
+      paid:false
     });
-
   }
 
   const payment =
@@ -21,16 +38,14 @@ export default async function handler(req, res) {
     );
 
   if (payment) {
-
     return res.status(200).json({
-      paid: true,
+      paid:true,
       payment
     });
-
   }
 
   return res.status(200).json({
-    paid: false
+    paid:false
   });
 
 }
